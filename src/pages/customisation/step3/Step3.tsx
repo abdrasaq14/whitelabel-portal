7533009bb2f7679d66798b1b9f838b649aa85b14
abdrasaq14 <abdrasaq14@gmail.com>
2024-07-22 16:32:50 +0100

    completion odal for stage 3

--- src/pages/customisation/step3/Step3.tsx
+++ src/pages/customisation/step3/Step3.tsx
@@ -13,7 +13,8 @@ import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
 import { CustomisationService } from "../../../services/customisation.service";
 import removeBackground from "../../../utils/removeBg";
-
+import { CompletionModal } from "../../../components/Modal/CompletionModal";
+import { useNavigate} from "react-router-dom";
 interface Step3Props {
   primaryColor: string;
   secondaryColor: string;
@@ -38,6 +39,7 @@ function Step3({ primaryColor, secondaryColor }: Step3Props) {
       scrollRef.current.scrollIntoView({ behavior: "smooth" });
     }
   };
+  const navigate = useNavigate();
   const handleImageUpload = useMutation(
     async (file: File) => {
       const formData = new FormData();
@@ -94,7 +96,7 @@ function Step3({ primaryColor, secondaryColor }: Step3Props) {
       }
 
       setUploadError("");
-      if (selectedTemplate !== 3) {
+      if (selectedTemplate !== 2) {
         const bgRemovedImage = await removeBackground(file);
         if (bgRemovedImage) {
           // Continue with your image upload logic, using the bgRemovedImage URL
@@ -106,11 +108,13 @@ function Step3({ primaryColor, secondaryColor }: Step3Props) {
         }
       }
       else {
+        console.log("yes")
         handleImageUpload.mutate(file);
+        return
       }
     }
   };
-
+console.log("selectedTemplate", selectedTemplate)
   const form = useFormik({
     initialValues: {
       heroText: "",
@@ -138,7 +142,8 @@ function Step3({ primaryColor, secondaryColor }: Step3Props) {
         setIsUploading(false);
         setUploadError("");
         form.setSubmitting(false);
-        console.log("response", response);
+        setIsOpen(true);
+        // console.log("response", response);
       },
       onError: (err: any) => {
         setIsUploading(false);
@@ -168,56 +173,59 @@ function Step3({ primaryColor, secondaryColor }: Step3Props) {
 
   console.log("form.Value", form.values);
   const formats = ["font", "bold", "italic", "underline", "strike", "color"];
+  const [isOpen, setIsOpen] = useState(false);
   return (
-    <div className="flex w-full bg-[#F3F3F3] h-full">
-      <div className="w-[50%] h-full bg-white text-foundation-black p-8 font-satoshiBold">
-        <h1 className="text-3xl mb-1">Customise your Account</h1>
-        <p className="mb-4 text-sm text-[#5a5a5a] font-satoshiRegular w-[60%]">
-          Choose a template and provide the hero section image and content below
-        </p>
-        {/* step */}
-        <div className="flex max-w-[10rem] gap-2 my-4">
-          <span className="h-2 w-1/3 bg-[#4b00821f] rounded-xl"></span>
-          <span className="h-2 w-1/3 bg-[#4b00821f] rounded-xl"></span>
-          <span className="h-2 w-2/3 bg-foundation-darkPurple rounded-xl"></span>
-        </div>
-        {/* templates */}
-        <div className="flex gap-4 max-w-[90%] my-6">
-          {templates.map((template, index) => (
-            <TemplateCard
-              key={index}
-              index={index}
-              image={template.image}
-              title={template.title}
-              selectedTemplate={selectedTemplate}
-              onClick={handleTemplateClick}
-            />
-          ))}
-        </div>
-        {/* hero section */}
-        <div className="max-w-[90%]  my-6">
-          <FormikProvider value={form}>
-            <form className="flex flex-col gap-4 font-satoshiMedium w-full">
-              <div className="rounded-lg p-4 border border-[#C8CCD0]">
-                <p className="font-satoshiBold text-lg mb-4"> Hero Section</p>
-                {/* hero text */}
-                <div className="flex flex-col gap-4 mb-4">
-                  <div className="flex flex-col">
-                    <span className="text-[#344054] mb-2 ">
-                      Hero section Text
-                    </span>
-                    <ReactQuill
-                      value={form.values.heroText}
-                      onChange={(content) =>
-                        form.setFieldValue("heroText", content)
-                      }
-                      modules={modules}
-                      formats={formats}
-                      placeholder="Provide your hero section text here..."
-                      className=" placeholder:text-sm placeholder:text-[#667085] text-sm min-h-[10rem] focus:outline-none overflow-hidden rounded-lg border border-[#C8CCD0] "
-                      theme="snow"
-                    />
-                    <style>{`
+    <>
+      <div className="flex w-full bg-[#F3F3F3] h-full">
+        <div className="w-[50%] h-full bg-white text-foundation-black p-8 font-satoshiBold">
+          <h1 className="text-3xl mb-1">Customise your Account</h1>
+          <p className="mb-4 text-sm text-[#5a5a5a] font-satoshiRegular w-[60%]">
+            Choose a template and provide the hero section image and content
+            below
+          </p>
+          {/* step */}
+          <div className="flex max-w-[10rem] gap-2 my-4">
+            <span className="h-2 w-1/3 bg-[#4b00821f] rounded-xl"></span>
+            <span className="h-2 w-1/3 bg-[#4b00821f] rounded-xl"></span>
+            <span className="h-2 w-2/3 bg-foundation-darkPurple rounded-xl"></span>
+          </div>
+          {/* templates */}
+          <div className="flex gap-4 max-w-[90%] my-6">
+            {templates.map((template, index) => (
+              <TemplateCard
+                key={index}
+                index={index}
+                image={template.image}
+                title={template.title}
+                selectedTemplate={selectedTemplate}
+                onClick={handleTemplateClick}
+              />
+            ))}
+          </div>
+          {/* hero section */}
+          <div className="max-w-[90%]  my-6">
+            <FormikProvider value={form}>
+              <form className="flex flex-col gap-4 font-satoshiMedium w-full">
+                <div className="rounded-lg p-4 border border-[#C8CCD0]">
+                  <p className="font-satoshiBold text-lg mb-4"> Hero Section</p>
+                  {/* hero text */}
+                  <div className="flex flex-col gap-4 mb-4">
+                    <div className="flex flex-col">
+                      <span className="text-[#344054] mb-2 ">
+                        Hero section Text
+                      </span>
+                      <ReactQuill
+                        value={form.values.heroText}
+                        onChange={(content) =>
+                          form.setFieldValue("heroText", content)
+                        }
+                        modules={modules}
+                        formats={formats}
+                        placeholder="Provide your hero section text here..."
+                        className=" placeholder:text-sm placeholder:text-[#667085] text-sm min-h-[10rem] focus:outline-none overflow-hidden rounded-lg border border-[#C8CCD0] "
+                        theme="snow"
+                      />
+                      <style>{`
                     .ql-editor{
                       min-height: 10rem;
                       
@@ -275,123 +283,130 @@ function Step3({ primaryColor, secondaryColor }: Step3Props) {
                         display: none; // Hide <br> after <p> tags to avoid extra spacing
                       }
                   `}</style>
-                    {/* <textarea
+                      {/* <textarea
                       placeholder="Provide your hero section text here..."
                       className="rounded-lg border border-[#C8CCD0] p-2 placeholder:text-sm placeholder:text-[#667085] text-sm max-h-[10rem] min-h-[10rem] focus:outline-none"
                       {...form.getFieldProps("heroText")}
                     ></textarea> */}
-                    <div
-                      className="flex "
-                      style={{
-                        justifyContent: form.errors.heroText
-                          ? "space-between"
-                          : "end"
-                      }}
-                    >
-                      {form.errors.heroText && (
-                        <span className="text-[#D42620] text-sm">
-                          {form.errors.heroText}
+                      <div
+                        className="flex "
+                        style={{
+                          justifyContent: form.errors.heroText
+                            ? "space-between"
+                            : "end"
+                        }}
+                      >
+                        {form.errors.heroText && (
+                          <span className="text-[#D42620] text-sm">
+                            {form.errors.heroText}
+                          </span>
+                        )}
+                        <span className="font-satoshiLight text-sm text-[#667085]">
+                          Max. of 70 Characters
                         </span>
-                      )}
-                      <span className="font-satoshiLight text-sm text-[#667085]">
-                        Max. of 70 Characters
-                      </span>
+                      </div>
                     </div>
                   </div>
-                </div>
-                {/* hero image */}
-                <div className="flex flex-col">
-                  <span className="text-[#344054] mb-2">
-                    Upload Hero section Image
-                  </span>
-                  {isUploading ? (
-                    <div className="flex flex-col gap-2 items-center justify-center rounded-lg h-[15rem] bg-[#f8f8ff]">
-                      <Spinner color="#4B0082" />
-                      <span className="text-sm text-[#676767]">
-                        Uploading image...
-                      </span>
-                    </div>
-                  ) : (
-                    <div className="flex flex-col cursor-pointer  items-center justify-center rounded-lg gap-2 relative border-[0.8px] border-dashed border-[#384eb74d] h-[15rem] bg-[#f8f8ff]">
-                      <div className="w-80px h-[50px]">
-                        <img
-                          src={uploadIcon}
-                          alt="uploadIcon"
-                          className="object-contain"
+                  {/* hero image */}
+                  <div className="flex flex-col">
+                    <span className="text-[#344054] mb-2">
+                      Upload Hero section Image
+                    </span>
+                    {isUploading ? (
+                      <div className="flex flex-col gap-2 items-center justify-center rounded-lg h-[15rem] bg-[#f8f8ff]">
+                        <Spinner color="#4B0082" />
+                        <span className="text-sm text-[#676767]">
+                          Uploading image...
+                        </span>
+                      </div>
+                    ) : (
+                      <div className="flex flex-col cursor-pointer  items-center justify-center rounded-lg gap-2 relative border-[0.8px] border-dashed border-[#384eb74d] h-[15rem] bg-[#f8f8ff]">
+                        <div className="w-80px h-[50px]">
+                          <img
+                            src={uploadIcon}
+                            alt="uploadIcon"
+                            className="object-contain"
+                          />
+                        </div>
+                        <span className=" font-satoshiBold text-lg text-[#333333]">
+                          Drag & drop files or{" "}
+                          <span className="text-foundation-darkPurple underline">
+                            Browse
+                          </span>
+                        </span>
+                        <span className="text-sm text-[#676767]">
+                          Supported formats: JPEG, PNG, JPG
+                        </span>
+                        <input
+                          type="file"
+                          className="cursor-pointer absolute opacity-0 h-full w-full"
+                          onChange={handleFileChange}
                         />
                       </div>
-                      <span className=" font-satoshiBold text-lg text-[#333333]">
-                        Drag & drop files or{" "}
-                        <span className="text-foundation-darkPurple underline">
-                          Browse
-                        </span>
-                      </span>
-                      <span className="text-sm text-[#676767]">
-                        Supported formats: JPEG, PNG, JPG
-                      </span>
-                      <input
-                        type="file"
-                        className="cursor-pointer absolute opacity-0 h-full w-full"
-                        onChange={handleFileChange}
-                      />
-                    </div>
-                  )}
+                    )}
 
-                  {uploadError ? (
-                    <span className="text-[#D42620] text-sm">
-                      {uploadError}
-                    </span>
-                  ) : (
-                    form.errors.heroImage && (
+                    {uploadError ? (
                       <span className="text-[#D42620] text-sm">
-                        {form.errors.heroImage}
+                        {uploadError}
                       </span>
-                    )
-                  )}
+                    ) : (
+                      form.errors.heroImage && (
+                        <span className="text-[#D42620] text-sm">
+                          {form.errors.heroImage}
+                        </span>
+                      )
+                    )}
+                  </div>
                 </div>
-              </div>
-              <button
-                type="button"
-                disabled={!form.values.heroText.trim() || !form.values.heroImage.trim()|| form.isSubmitting || isUploading}
-                onClick={() => form.handleSubmit()}
-                className="bg-primary w-full rounded-lg text-white text-sm inline-flex gap-2 my-4 items-center justify-center text-center p-2.5 font-medium disabled:bg-opacity-50 disabled:cursor-not-allowed"
-              >
-                {form.isSubmitting ? (
-                  <Spinner />
-                ) : (
-                  <>
-                    Continue{" "}
-                    <span>
-                      <MdOutlineArrowForward size={16} />
-                    </span>
-                  </>
-                )}
-              </button>
-            </form>
-          </FormikProvider>
-        </div>
-      </div>
-      <div className="w-[50%] h-full bg-foundation-darkPurple p-6 flex items-center justify-center">
-        <div className="bg-white h-full w-[95%] flex flex-col">
-          <div className="h-[5rem] w-full">
-            <div
-              className={`h-[50%] w-full`}
-              style={{ backgroundColor: primaryColor }}
-            ></div>
-          </div>
-          <div className="p-4 border-[2px] border-dotted border-[#D42620] scale-105 bg-white shadow-md  animate-zoomOut">
-            <BannerTemplate
-              scrollRef={scrollRef}
-              template={selectedTemplate}
-              heroText={form.values.heroText}
-              heroImage={form.values.heroImage}
-              primaryColor={primaryColor}
-              secondaryColor={secondaryColor}
-            />
+                <button
+                  type="button"
+                  disabled={
+                    !form.values.heroText.trim() ||
+                    !form.values.heroImage.trim() ||
+                    form.isSubmitting ||
+                    isUploading
+                  }
+                  onClick={() => form.handleSubmit()}
+                  className="bg-primary w-full rounded-lg text-white text-sm inline-flex gap-2 my-4 items-center justify-center text-center p-2.5 font-medium disabled:bg-opacity-50 disabled:cursor-not-allowed"
+                >
+                  {form.isSubmitting ? (
+                    <Spinner />
+                  ) : (
+                    <>
+                      Continue{" "}
+                      <span>
+                        <MdOutlineArrowForward size={16} />
+                      </span>
+                    </>
+                  )}
+                </button>
+              </form>
+            </FormikProvider>
           </div>
         </div>
+        <div className="w-[50%] h-full bg-foundation-darkPurple p-6 flex items-center justify-center">
+          <div className="bg-white h-full w-[95%] flex flex-col">
+            <div className="h-[5rem] w-full">
+              <div
+                className={`h-[50%] w-full`}
+                style={{ backgroundColor: primaryColor }}
+              ></div>
+            </div>
+            <div className="p-4 border-[2px] border-dotted border-[#D42620] scale-105 bg-white shadow-md  animate-zoomOut">
+              <BannerTemplate
+                scrollRef={scrollRef}
+                template={selectedTemplate}
+                heroText={form.values.heroText}
+                heroImage={form.values.heroImage}
+                primaryColor={primaryColor}
+                secondaryColor={secondaryColor}
+              />
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
+      <CompletionModal isOpen={isOpen} handleProceed={()=>navigate("/dashboard")} />
+    </>
   );
 }
 
