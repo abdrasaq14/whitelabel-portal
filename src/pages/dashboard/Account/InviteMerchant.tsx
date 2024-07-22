d9078e6d54f7f69a91da621351cd392bb3cd50c9
abdrasaq14 <abdrasaq14@gmail.com>
2024-07-22 14:52:06 +0100

    invite merchant

--- src/pages/dashboard/Account/InviteMerchant.tsx
+++ src/pages/dashboard/Account/InviteMerchant.tsx
@@ -1,9 +1,25 @@
 import { IoCopyOutline } from "react-icons/io5";
-
+import { useAuth } from "../../../zustand/auth.store";
+import { toast } from "react-hot-toast";
 function InviteMerchant() {
+    const whiteLabelName = useAuth(
+    // @ts-ignore
+    (s) => s.profile.whiteLabelName
+  );
+ const handleCopyLink = () => {
+   const textToCopy = `https://www.profitall.co.uk/onboarding?whiteLabelName=${whiteLabelName}`;
+   navigator.clipboard
+     .writeText(textToCopy)
+     .then(() => {
+        toast.success("Link copied to clipboard");
+     })
+     .catch((err) => {
+       console.error("Failed to copy: ", err);
+     });
+ };
   return (
-    <div className="flex flex-col sm:flex-row flex-wrap gap-4 ">
-      <div className=" basis-2/3 flex-grow">
+    <div className="flex flex-col sm:flex-row flex-wrap gap-6 ">
+      <div className=" basis-2/3 flex-grow ">
         <h2 className="text-base font-satoshiBold  font-semibold text-primary-text">
           Invite Merchant
         </h2>
@@ -17,12 +33,13 @@ function InviteMerchant() {
         </p>
       </div>
 
-      <div className="basis 1/3 flex items-start justify-center">
+      <div className="basis 1/3 min-w-[10%] flex items-center justify-center">
         <button
           type="button"
-          className="rounded-lg px-16 py-8 flex items-center justify-center max-w-[140px] text-[#2B2C34]"
+          onClick={handleCopyLink}
+          className="rounded-lg p-4 flex gap-2 items-center justify-center border-[0.45px] border-foundation-darkPurple max-h-[40px] max-w-[140px] text-foundation-darkPurple hover:bg-foundation-darkPurple hover:text-white"
         >
-          Copy <IoCopyOutline className="text-foundation-darkPurple" />
+          Copy <IoCopyOutline />
         </button>
       </div>
     </div>
