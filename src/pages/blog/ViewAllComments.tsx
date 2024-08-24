e6bbd4cf23ffa626e568480608eb34af7b649750
abdrasaq14 <abdrasaq14@gmail.com>
2024-08-24 18:47:10 +0100

    all comments page in progress

--- src/pages/blog/ViewAllComments.tsx
+++ src/pages/blog/ViewAllComments.tsx
@@ -4,24 +4,24 @@ function ViewAllComments() {
   return (
     <div className="flex flex-col gap-8">
       <div className="flex flex-wrap gap-4 xl:grid xl:grid-cols-3 xl:items-start xl:justify-start xl:gap-6">
-        {posts.map((blog: BlogPayload, index: number) => (
+        {/* {posts.map((blog: BlogPayload, index: number) => (
           <CommentCard
             key={index}
             comment={comment}
             showDeleteIcon={activeTab === "all" ? true : false}
             handleDelete={() => handleDeleteComment(comment._id as string)}
           />
-        ))}
+        ))} */}
       </div>
 
-      <Pagination
+      {/* <Pagination
         total={total}
         limit={limit}
         page={currentPage}
         onPageChange={handlePagination}
         increase={handleNext}
         decrease={handlePrevious}
-      />
+      /> */}
     </div>
   );
 }
