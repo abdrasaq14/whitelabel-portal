d3ede5fbc9e71fb253708f553637ff16035a7cf9
Oladepo Olushina <oladepo.olushina@gmail.com>
2024-10-02 15:44:17 +0100

    Update

--- src/pages/customisation/Setup/DummyProduct.ts
+++ src/pages/customisation/Setup/DummyProduct.ts
@@ -1,55 +1,55 @@
 const inventory: any[] = [
   {
-    name: "Ainvoer green hand bag",
+    name: "Ainvoer green bag",
     storeName: "MS Boutique",
     image: "/images/item1.svg",
     price: "16,500",
     rating: "/images/rating.svg"
   },
   {
-    name: "Bar chair with Suede",
+    name: "Bar chair",
     storeName: "M&K Furniture store",
     image: "/images/item2.svg",
     price: "16,500",
     rating: "/images/rating.svg"
   },
   {
-    name: "Club chair with Suede",
+    name: "Club chair",
     storeName: "T-classic Furniture Store",
     image: "/images/item3.svg",
     price: "16,500",
     rating: "/images/rating.svg"
   },
   {
-    name: "Ainvoer sunglasses & Purse",
+    name: "Ainvoer sunglasses",
     storeName: "MS Boutique",
     image: "/images/item4.svg",
     price: "16,500",
     rating: "/images/rating.svg"
   },
   {
-    name: "Turtleneck Wool Carrigan",
+    name: "Turtleneck Wool ",
     storeName: "High Tension boutique",
     image: "/images/item5.svg",
     price: "16,500",
     rating: "/images/rating.svg"
   },
   {
-    name: "Ainvoer green hand bag",
+    name: "Ainvoer green bag",
     storeName: "MS boutique",
     image: "/images/item6.svg",
     price: "16,500",
     rating: "/images/rating.svg"
   },
   {
-    name: "Turtleneck Wool Carrigan",
+    name: "Turtleneck Wool",
     storeName: "High Tension boutique",
     image: "/images/item7.svg",
     price: "16,500",
     rating: "/images/rating.svg"
   },
   {
-    name: "Bar chair with Suede",
+    name: "Bar chair",
     storeName: "M&K Furniture store",
     image: "/images/item8.svg",
     price: "16,500",
