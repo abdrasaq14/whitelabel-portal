de24b8ed4505a6e87421f55f4e8b7ce4184e7733
abdrasaq14 <abdrasaq14@gmail.com>
2024-11-08 10:44:08 +0100

    blog module wrap up

--- src/customHooks/useStorage.ts
+++ src/customHooks/useStorage.ts
@@ -27,7 +27,7 @@ const useStorage = () => {
 
     const updateLocalData = (key: string, prop: string, value: any) => {
         
-        if(!isClient) return null;
+        if (!isClient) return null;
 
         const data = getLocalData(key);
 
@@ -81,7 +81,7 @@ const useStorage = () => {
 
     const updateSessionData = (key: string, prop: string, value: any) => {
         
-        if(!isClient) return null;
+        if (!isClient) return null;
 
         const data = getSessionData(key);
 
@@ -94,6 +94,7 @@ const useStorage = () => {
         return data;
 
     }
+    
 
     const removeSessionData = (key: string) => {
     
@@ -143,6 +144,5 @@ console.log("SessionData", currentUser, storeSessionData);
         updateSessionData
     
     };
-};
-
+}
 export default useStorage;
