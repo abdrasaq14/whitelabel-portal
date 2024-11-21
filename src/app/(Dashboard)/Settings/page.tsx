0b76600f12603a79181509424b0085e6cb0751b9
Emekaag <emeka@profitall.co.uk>
2024-11-21 08:23:17 +0100

    "Added date-fns dependency, updated package-lock.json and package.json, modified DiscoverProducts and Settings pages, updated layout and sidenav components, and added orderReducer to store"

--- src/app/(Dashboard)/Settings/page.tsx
+++ src/app/(Dashboard)/Settings/page.tsx
@@ -25,7 +25,7 @@ const page = () => {
 
         { label: 'security', content: <Security /> },
 
-        { label: 'settings', content: <OtherSettings /> },
+        // { label: 'settings', content: <OtherSettings /> },
 
 
     ];
--- src/app/(Dashboard)/order/page.tsx
+++ src/app/(Dashboard)/order/page.tsx
@@ -5,6 +5,7 @@ const page = () => {
   return (
     <OrderTransaction />
   )
+
 }
 
 export default page
